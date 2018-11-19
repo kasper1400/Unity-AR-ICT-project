de99e9eb8637892bf2d498eb943337666cf96fb3
Marinela <beuca.marinela@yahoo.com>
2018-11-19 12:22:14 +0200

    firsversion

---
+++ AR1/Assets/ButtonManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ARScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Info()
+    {
+        Debug.Log("Info button clicked");
+    }
+
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+}
