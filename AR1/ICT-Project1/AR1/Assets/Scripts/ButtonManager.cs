7eb9bc9b05fe584d066ddfbfa18ee8fa7385d7ab
kasper1400 <kasper.forsstrom@student.hamk.fi>
2018-11-19 14:02:28 +0200

    fix

---
+++ AR1/ICT-Project1/AR1/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ARScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Info()
+    {
+        SceneManager.LoadScene("InfoScene");
+    }
+
+    public void ExitApp()
+    {
+        Application.OpenURL("https://lukkarit.hamk.fi/");
+    }
+}
---
+++ AR1/ICT-Project1/AR1/Assets/Scripts/SplashScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(splashLoader());
+    }
+
+    IEnumerator splashLoader()
+    {
+        yield return new WaitForSeconds(4.5f);
+        SceneManager.LoadScene("Menu");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
