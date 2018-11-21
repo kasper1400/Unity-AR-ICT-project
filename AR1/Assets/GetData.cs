a29ef12b048395dc2042aa04fb400901d89f9dc5
kasper1400 <kasper.forsstrom@student.hamk.fi>
2018-11-21 13:08:08 +0200

    Database connection basic codes

---
+++ AR1/Assets/GetData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetData : MonoBehaviour {
+
+    public string URL;
+    public Text ID_text;
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(GetDataFromServer());
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    IEnumerator GetDataFromServer(){
+
+        WWWForm formData = new WWWForm();
+        formData.AddField("userName", "ucSrzhL6ojWEXotgiOWM");
+
+        WWW www;
+        www = new WWW(URL);
+        yield return www;
+
+        string json = www.text;
+
+        if (www.isDone)
+        {
+            Building data = JsonUtility.FromJson<Building>(json);
+            ID_text.text = data.id;
+        }
+    }
+
+    class Building
+    {
+        public string id;
+        public string type;
+    }
+}
