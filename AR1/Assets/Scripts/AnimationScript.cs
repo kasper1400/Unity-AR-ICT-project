860cf63ce012c04ba9a3a9cb97738fdaed0d2527
Kasper <kasper1400@student.hamk.fi>
2018-11-28 17:08:01 +0200

    Playing blender animations in Unity

---
+++ AR1/Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour {
+
+    public Animation anim;
+
+    void Start()
+    {
+        //gameObject.GetComponent<Animator>().Play("bike");
+        anim = GetComponent<Animation>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        anim.Play("bike");
+    }
+}
