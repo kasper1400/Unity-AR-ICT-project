848b7af29392cd6f58c71cd4507ef355a8b189ac
kasper1400 <kasper.forsstrom@student.hamk.fi>
2018-12-04 16:42:22 +0200

    Tracking imagetarget in code

--- AR1/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
+++ AR1/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -19,7 +19,7 @@ using UnityEngine.Video;
 /// Changes made to this file could be overwritten when upgrading the Vuforia version. 
 /// When implementing custom event handler behavior, consider inheriting from this class instead.
 /// </summary>
-public class CustomDefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
 {
     #region PROTECTED_MEMBER_VARIABLES
 
